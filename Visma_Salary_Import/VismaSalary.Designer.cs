6e908045e7fdce5100e10bd9675683d555f76917
einaroso <einar.palsson@mindcamp.se>
2016-11-23 13:36:54 +0100

    Add project files.

---
+++ Visma_Salary_Import/VismaSalary.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Visma_Salary_Import
+{
+    partial class SalaryXML
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StartDate = new System.Windows.Forms.DateTimePicker();
+            this.EndDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start Date";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "End Date";
+            // 
+            // StartDate
+            // 
+            this.StartDate.CustomFormat = "yyyy-MM-dd";
+            this.StartDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.StartDate.Location = new System.Drawing.Point(85, 27);
+            this.StartDate.Name = "StartDate";
+            this.StartDate.Size = new System.Drawing.Size(93, 20);
+            this.StartDate.TabIndex = 5;
+            // 
+            // EndDate
+            // 
+            this.EndDate.CustomFormat = "yyyy-MM-dd";
+            this.EndDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.EndDate.Location = new System.Drawing.Point(85, 65);
+            this.EndDate.Name = "EndDate";
+            this.EndDate.Size = new System.Drawing.Size(93, 20);
+            this.EndDate.TabIndex = 6;
+            // 
+            // SalaryXML
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 167);
+            this.Controls.Add(this.EndDate);
+            this.Controls.Add(this.StartDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "SalaryXML";
+            this.Text = "Create Salary XML";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker StartDate;
+        private System.Windows.Forms.DateTimePicker EndDate;
+    }
+}
+
---
+++ Visma_Salary_Import/VismaSalary.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Xml;
+using System.IO;
+using System.Configuration;
+
+namespace Visma_Salary_Import
+{
+    public partial class SalaryXML : Form
+    {
+        public SalaryXML()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTimePicker dtStart = new DateTimePicker();
+            dtStart.Value = StartDate.Value;
+            DateTimePicker dtEnd = new DateTimePicker();
+            dtEnd.Value = EndDate.Value;
+            if (dtStart.Value > dtEnd.Value)
+            {
+                MessageBox.Show("Start Date has to before end date.");
+                return;
+            }
+
+            int enddate = Int32.Parse(dtEnd.Value.ToString("yyyyMMdd"));
+            int startdate = Int32.Parse(dtStart.Value.ToString("yyyyMMdd"));
+            CreateXMLStoreProcedure(enddate, startdate);
+            //UpdateAggr(enddate);
+            //UpdateFreeInf1(enddate);
+
+        }
+
+
+        public static void CreateXMLStoreProcedure(int vEndDate, int vStartDate)
+        {
+            //String sConnection = "Data Source=mindsql\\mindsql;Initial Catalog=F0001;Persist Security Info=False;User ID=LonXML;Password=Vinter2016!;";
+            String sConnection = System.Configuration.ConfigurationManager.ConnectionStrings["VismaSalary"].ConnectionString;
+            String filename = System.Configuration.ConfigurationManager.AppSettings["filename"];
+            SqlConnection mySqlConnection = new SqlConnection(sConnection);
+            mySqlConnection.Open();
+
+            // 1. Get the same data through the provider.
+            SqlCommand cmd = new SqlCommand("SalaryCreateXML", mySqlConnection);
+
+            // 2. set the command object so it knows to execute a stored procedure
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            // 3. add parameter to command, which will be passed to the stored procedure
+            cmd.Parameters.Add(new SqlParameter("@EndDate", vEndDate));
+            cmd.Parameters.Add(new SqlParameter("@StartDate", vStartDate));
+            
+            using (XmlReader reader = cmd.ExecuteXmlReader())
+            {
+                XmlDocument dom = new XmlDocument();
+                dom.Load(reader);
+
+                var settings = new XmlWriterSettings();
+                settings.Indent = true;
+                settings.OmitXmlDeclaration = false;
+                settings.ConformanceLevel = ConformanceLevel.Document;
+                settings.Encoding = UTF8Encoding.UTF8;
+
+                //using (var writer = XmlTextWriter.Create("c:/temp/file2.xml", settings))
+                using (var writer = XmlTextWriter.Create(filename, settings))
+                {
+                    dom.WriteContentTo(writer);
+                }
+            }
+
+            // Write data to files: data1.xml and data2.xml for comparison.
+            MessageBox.Show("The file " + filename + " has been created.");
+
+        }
+
+
+
+        public static void UpdateFreeInf1(int vEndDate)
+        {
+            //String sConnection = "Data Source=mindsql\\mindsql;Initial Catalog=F0001;Persist Security Info=False;User ID=LonXML;Password=Vinter2016!;";
+            String sConnection = System.Configuration.ConfigurationManager.ConnectionStrings["VismaSalary"].ConnectionString;
+            SqlConnection mySqlConnection = new SqlConnection(sConnection);
+            mySqlConnection.Open();
+
+            // Get the same data through the provider.
+            // Use enddate
+            String commandText = "update dbo.FreeInf1 set val5=1 where InfCatNo=3 and Val4=1 and Val5=0 and Dt1 < @EndDate";
+            SqlCommand command = new SqlCommand(commandText, mySqlConnection);
+            command.Parameters.Add("@EndDate", SqlDbType.Int);
+            command.Parameters["@EndDate"].Value = vEndDate;
+
+            int rows = command.ExecuteNonQuery();
+
+        }
+
+        public static void UpdateAggr(int vEndDate, int vStartDate)
+        {
+            //String sConnection = "Data Source=mindsql\\mindsql;Initial Catalog=F0001;Persist Security Info=False;User ID=LonXML;Password=Vinter2016!;";
+            String sConnection = System.Configuration.ConfigurationManager.ConnectionStrings["VismaSalary"].ConnectionString;
+            SqlConnection mySqlConnection = new SqlConnection(sConnection);
+            mySqlConnection.Open();
+
+            // Get the same data through the provider.
+            // Use enddate
+            String commandText = "set c.FinDt = " + vEndDate + " from Agr c inner join Prod P on c.ProdNo = P.ProdNo inner join Ord O on c.ProdNo = O.OrdNo  where P.ProdPrGr = 4 and c.ProdNo not in ('099', '059') and O.DelDt <= c.FinDt and c.FinDt < @EndDate and c.FinDt > @StartDate";
+            SqlCommand command = new SqlCommand(commandText, mySqlConnection);
+            command.Parameters.Add("@EndDate", SqlDbType.Int);
+            command.Parameters["@EndDate"].Value = vEndDate;
+            command.Parameters.Add("@StartDate", SqlDbType.Int);
+            command.Parameters["@StartDate"].Value = vStartDate;
+
+            int rows = command.ExecuteNonQuery();
+
+        }
+
+        static void GetConnectionStrings()
+        {
+            ConnectionStringSettingsCollection settings =
+                ConfigurationManager.ConnectionStrings;
+
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    Console.WriteLine(cs.Name);
+                    Console.WriteLine(cs.ProviderName);
+                    Console.WriteLine(cs.ConnectionString);
+                }
+            }
+        }
+    }
+     }
